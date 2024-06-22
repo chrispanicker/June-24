b5f0af0c811b3316db872f0e678767c59fa9214b
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2024-06-22 10:06:44 -0400

    testing...

--- sketch.js
+++ sketch.js
@@ -11,12 +11,20 @@ function setup(){
 
   if(width<900){
       w=5;
-      fontSize=80;
-      points = font.textToPoints(msg, txtX, txtY, fontSize, {sampleFactor: .02});
-  }else{
+      fontSize=140;
+      points = font.textToPoints(msg, txtX, txtY, fontSize, {sampleFactor: .1});
+  }else if(width<1100){
+    w=10;
+    fontSize=200;
+    points = font.textToPoints(msg, txtX, txtY, fontSize, {sampleFactor: .16});
+  }else if(width<1440){
       w=10;
-      fontSize=200;
+      fontSize=300;
       points = font.textToPoints(msg, txtX, txtY, fontSize, {sampleFactor: .16});
+  }else{
+    w=10;
+    fontSize=350;
+    points = font.textToPoints(msg, txtX, txtY, fontSize, {sampleFactor: .2});
   }
   
   let bounds = font.textBounds(msg, txtX, txtY, fontSize);
@@ -35,16 +43,17 @@ function setup(){
 
 function draw(){
   let wave=0;
-  let senstivities = .1
+  let senstivities = .04
   background(0)
   stroke(255)
+  strokeWeight(2)
   noFill()
   micLevel = mic.getLevel();
   push()
   for(let i=0; i<width; i++){
     let amt = map(i,0,width, 0,1)
-    let color1 = color(map(micLevel, 0, .3, 0, 255), 0, map(micLevel, 0, .3, 200, 255))
-    let color2 = color(map(micLevel, 0, .3, 100, 255), map(micLevel, 0, .3, 200, 255), 0)
+    let color1 = color(map(micLevel, 0, senstivities, 0, 255), 0, map(micLevel, 0, senstivities, 200, 255))
+    let color2 = color(map(micLevel, 0, senstivities, 100, 255), map(micLevel, 0, senstivities, 200, 255), 0)
     let myColor = lerpColor(color1, color2, amt)
     stroke(myColor)
     line(i, 0, i,height)
@@ -54,7 +63,7 @@ function draw(){
   beginShape()
   for(let pt of points){
     if(pt.y<height/2){
-      let y = map(micLevel, 0, senstivities, height/2, pt.y);
+      let y = map(micLevel, 0, .01, height/2, pt.y);
       ellipse(pt.x, random(y, pt.y), 10*micLevel*3)
       vertex(pt.x, random(y, pt.y))
     }
@@ -65,11 +74,10 @@ function draw(){
     if(pt.y<height/2){
 
     }else{
-      let y = map(micLevel, 0, senstivities, height/2, pt.y);
+      let y = map(micLevel, 0, .01, height/2, pt.y);
       ellipse(pt.x, random(y, pt.y), 10*micLevel*3)
       vertex(pt.x, random(y, pt.y))
     }
   }
   endShape()
 }
-
